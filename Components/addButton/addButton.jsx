908508782ee51aeed1f7c0d6900aa0bfcb6be2d3
PeterThere <259087@student.pwr.edu.pl>
2023-11-14 10:03:03 +0100

    Pretty selector

--- Components/addButton/addButton.jsx
+++ Components/addButton/addButton.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import * as data from "../../data/symbols.json";
@@ -12,36 +12,70 @@ export const AddButton = () => {
     }
   };
   return (
-    <SelectDropdown
-      data={data.symbols}
-      onSelect={(selectedItem) => {
-        console.log(selectedItem);
-      }}
-      defaultButtonText={"Select country"}
-      buttonTextAfterSelection={(selectedItem) => {
-        return selectedItem;
-      }}
-      rowTextForSelection={(item) => {
-        return item;
-      }}
-      search
-      renderDropdownIcon={(isOpened) => {
-        return (
-          <FontAwesome
-            name={isOpened ? "chevron-up" : "chevron-down"}
-            color={"#444"}
-            size={18}
-          />
-        );
-      }}
-      dropdownIconPosition={"right"}
-      searchPlaceHolder={"Search here"}
-      searchPlaceHolderColor={"darkgrey"}
-      renderSearchInputLeftIcon={() => {
-        return <FontAwesome name={"search"} color={"#444"} size={18} />;
-      }}
-    />
+    <View style={{ marginTop: 40 }}>
+      <SelectDropdown
+        data={data.symbols}
+        onSelect={(selectedItem) => {
+          console.log(selectedItem);
+        }}
+        defaultButtonText={"Select currency"}
+        buttonTextAfterSelection={(selectedItem) => {
+          return selectedItem;
+        }}
+        rowTextForSelection={(item) => {
+          return item;
+        }}
+        buttonStyle={styles.dropdown1BtnStyle}
+        buttonTextStyle={styles.dropdown1BtnTxtStyle}
+        dropdownStyle={styles.dropdown1DropdownStyle}
+        rowStyle={styles.dropdown1RowStyle}
+        rowTextStyle={styles.dropdown1RowTxtStyle}
+        selectedRowStyle={styles.dropdown1SelectedRowStyle}
+        search
+        searchInputStyle={styles.dropdown1searchInputStyleStyle}
+        searchPlaceHolder={"Search here"}
+        renderDropdownIcon={(isOpened) => {
+          return (
+            <FontAwesome
+              name={isOpened ? "chevron-up" : "chevron-down"}
+              color={"#444"}
+              size={18}
+            />
+          );
+        }}
+        dropdownIconPosition={"right"}
+        searchPlaceHolder={"Search here"}
+        searchPlaceHolderColor={"darkgrey"}
+        renderSearchInputLeftIcon={() => {
+          return <FontAwesome name={"search"} color={"#444"} size={18} />;
+        }}
+      />
+    </View>
   );
 };
+const styles = StyleSheet.create({
+  dropdown1BtnStyle: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#444",
+  },
+  dropdown1BtnTxtStyle: { color: "#444", textAlign: "left" },
+  dropdown1DropdownStyle: { backgroundColor: "#EFEFEF" },
+  dropdown1RowStyle: {
+    backgroundColor: "#EFEFEF",
+    borderBottomColor: "#C5C5C5",
+  },
+  dropdown1RowTxtStyle: { color: "#444", textAlign: "left" },
+  dropdown1SelectedRowStyle: { backgroundColor: "rgba(0,0,0,0.1)" },
+  dropdown1searchInputStyleStyle: {
+    backgroundColor: "#EFEFEF",
+    borderRadius: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: "#444",
+  },
+});
 
 export default AddButton;
