deee56d8952967e9a27c37ccfaeb8e6e998f223d
PeterThere <259087@student.pwr.edu.pl>
2023-11-14 09:33:08 +0100

    json log

--- Components/addButton/addButton.jsx
+++ Components/addButton/addButton.jsx
@@ -4,12 +4,21 @@ import SelectDropdown from "react-native-select-dropdown";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import * as data from "../../data/symbols.json";
+import * as fs from "react-native-fs";
 export const AddButton = () => {
   return (
     <SelectDropdown
       data={data.symbols}
       onSelect={(selectedItem) => {
-        console.log(selectedItem);
+        data[selectedItem] = { value: 0 };
+        fs.writeFile(
+          "./data/currencies.json",
+          JSON.stringify(newData),
+          console.log(selectedItem),
+          (err) => {
+            if (err) console.log("Error writing file:", err);
+          }
+        );
       }}
       defaultButtonText={"Select country"}
       buttonTextAfterSelection={(selectedItem) => {
