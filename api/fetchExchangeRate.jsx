e0b796e544ab4a8a636a3f1f8f613e7c1e5fc8dd
PeterThere <259087@student.pwr.edu.pl>
2023-11-26 22:42:39 +0100

    fetchExchangeRate działa, zapisanie do storage nie

--- api/fetchExchangeRate.jsx
+++ api/fetchExchangeRate.jsx
@@ -1,30 +1,33 @@
 import axios from "axios";
 import { AsyncStorage } from "react-native";
 
-const baseUrl = "https://api.exchangeratesapi.io";
+const baseUrl = "http://api.exchangeratesapi.io";
 
 _storeData = async (symbol, rate) => {
   try {
+    //nie zapisuje
     await AsyncStorage.setItem(symbol, rate);
+    console.log(symbol + " saved at " + rate);
   } catch (error) {
     console.log("Better luck next time!");
   }
 };
-//TO DO: change config to accept base, symbols as parameters
-const config = {
-  url: `${baseUrl}/v1/latest`,
-  method: "GET",
-  params: {
-    access_key: "4ab8f6af98f8b5b3c8dab0b795290a2f",
-    base: "PLN",
-    symbols: "EUR, USD",
-  },
-};
 
-axios(config).then((response) => {
-  console.log("Response from request: " + response.data);
-  response.data.rates.map((symbol, rate) => {
-    _storeData(symbol, rate);
-    console.log(symbol + "saved at" + rate);
+export const fetchExchangeRate = async (baza, symbolsList) => {
+  axios({
+    url: `${baseUrl}/v1/latest`,
+    method: "GET",
+    params: {
+      access_key: "4ab8f6af98f8b5b3c8dab0b795290a2f",
+      base: baza,
+      symbols: symbolsList,
+    },
+  }).then((response) => {
+    console.log(
+      "Response from request: " + JSON.stringify(response.data.rates)
+    );
+    Object.entries(response.data.rates).forEach(([symbol, rate]) => {
+      _storeData(symbol, rate);
+    });
   });
-});
+};
