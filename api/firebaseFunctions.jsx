c0db536578ebf85d148c292f38543cb8c6d5335a
PeterThere <259087@student.pwr.edu.pl>
2023-11-26 22:05:22 +0100

    getCurrencyMap() dziala i huczy

--- api/firebaseFunctions.jsx
+++ api/firebaseFunctions.jsx
@@ -1,16 +1,22 @@
-import { doc, setDoc, getDocs, collection } from "firebase/firestore";
+import { doc, setDoc, collection, getDocs } from "firebase/firestore";
 import { FIRESTORE_DB } from "../firebaseConfig";
 
+//Tworzy dokument o nazwie {symbol}, z polem value o wartości {value}
 export const setCurrencyAmount = async (symbol, value) => {
   await setDoc(doc(FIRESTORE_DB, "rates", symbol), {
     value: value,
   });
   console.log(symbol + "saved at: " + value);
 };
-// WIP
-// export const getCurrencyList = async () => {
-//   const docSnap = await getDocs(collection(FIRESTORE_DB, "rates"));
-//   if (docSnap) {
-//     console.log("Waluty:", docSnap);
-//   }
-// };
+
+//Zwraca mapę z kluczem(symbol waluty) i wartością(ilość waluty w skarbonce) eg. {"PLN" => 100}
+export const getCurrencyMap = async () => {
+  const data = new Map();
+  await getDocs(collection(FIRESTORE_DB, "rates")).then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      data.set(doc.id, doc.data().value);
+    });
+  });
+  console.log(data);
+  return data;
+};
