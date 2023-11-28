65f445ea39e97b3aca3be819a909ea90d14d86d5
PeterThere <259087@student.pwr.edu.pl>
2023-11-28 01:23:33 +0100

    fixed base fr fr

--- api/firebaseFunctions.jsx
+++ api/firebaseFunctions.jsx
@@ -32,12 +32,13 @@ export const getCurrencyMap = async () => {
   let data = {};
   await getDocs(collection(FIRESTORE_DB, "rates")).then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      if (doc.id != "base")
+      if (doc.id != "base") {
         //XD
         data[doc.id] = {
           fullName: doc.data().fullName,
           value: doc.data().value,
         };
+      }
     });
   });
   // console.log(data);
